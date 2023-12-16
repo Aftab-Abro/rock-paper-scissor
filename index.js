d370a62b4d641c9ba079e915a6f3729fe412c49f
Aftab-Abro <aftabalyabro@gmail.com>
2023-12-17 01:42:37 +0500

    Implement playRound function in index.js
    The playRound function compares the player's
    selection with the computer's selection and determines
    the winner of a round in the Rock, Paper, Scissors
    game. It updates the roundWinner variable accordingly,
    increments scores, and handles ties by replaying the round.

--- index.js
+++ index.js
@@ -37,5 +37,51 @@ function getPlayerChoice(){
   
 }
 
+function playRound(playerSelection, computerSelection) {
+  let roundWinner;   //  stores roundWinner of a program
+
+  if((playerSelection==='rock') && (computerSelection === 'rock') ){
+        roundWinner = 'tie';
+  }
+  else if ((playerSelection==='rock') && (computerSelection === 'paper') ){
+        roundWinner = 'You lose! Paper beats Rock';
+        computerScore++;
+  }
+  else if ((playerSelection==='rock') && (computerSelection === 'scissor') ){
+        roundWinner = 'You Won! Rock beats Scissor';
+        playerScore++;
+  }
+  else if ((playerSelection==='paper') && (computerSelection === 'paper') ){
+    roundWinner ='tie';
+  }
+  else if ((playerSelection==='paper') && (computerSelection === 'rock') ){
+    roundWinner = 'You won! Paper beats Rock';
+    playerScore++;
+  }
+  else if ((playerSelection==='paper') && (computerSelection === 'scissor') ){
+    roundWinner = 'You Lose! Scissor beats Paper';
+    computerScore++;
+  }
+  else if ((playerSelection==='scissor') && (computerSelection === 'scissor') ){
+    roundWinner = 'tie';
+  }
+  else if ((playerSelection==='scissor') && (computerSelection === 'paper') ){
+    roundWinner = 'You Won! Scissor beats Paper';
+    playerScore++;
+  }
+  else if ((playerSelection==='scissor') && (computerSelection === 'rock') ){
+   roundWinner = 'You Lose! Rock beats scissor';
+   computerScore++;
+  }
+  // replaying round if roundWinner equals tie
+  if(roundWinner === 'tie'){
+    console.log('roundWinner Tie \nReplaying Round');
+    playRound(getPlayerChoice(),getComputerChoice());
+  }
+  else
+   console.log(roundWinner);
+}
+
+
 
 
